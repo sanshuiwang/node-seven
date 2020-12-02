fd4d8a60d0fe2b38891350e6986837edcc68633c
sanshuiwang <768188667@qq.com>
2020-12-02 21:12:58 +0800

    demo03

---
+++ express/demo03/src/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const app = express();
+
+// app.use((req, res) => {
+//   res.json({
+//     name: "汪洋",
+//   });
+// });
+
+app.get("/name/:age", (req, res) => {
+  let { age } = req.params;
+  res.json({
+    name: "Ocean",
+    age,
+  });
+  // res.send("汪洋&Ocean");
+});
+
+app.post("/name", (req, res) => {
+  res.send("汪洋&Ocean/post");
+});
+
+app.listen(3000, () => {
+  console.log("服务启动成功");
+});
